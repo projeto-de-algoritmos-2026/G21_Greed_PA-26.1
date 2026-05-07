949e027f2e4b0a72758afacd355a1ff7b0078a46
MIGUEL ARTHUR <miguell1040z@gmail.com>
2026-05-07 19:38:20 -0300

    feat: [Miguel] implementar algoritmo interval partitioning

--- engine.js
+++ engine.js
@@ -20,3 +20,37 @@ class Job {
 function sortJobsByStartTime(jobs) {
     return jobs.sort((a, b) => a.startTime - b.startTime);
 }
+
+/**
+ * Implementa o algoritmo de Interval Partitioning.
+ * @param {Job[]} jobs - Array de jobs não processados.
+ * @returns {Job[]} - Array de jobs com resourceId atualizado.
+ */
+function intervalPartitioning(jobs) {
+    if (!jobs || jobs.length === 0) return [];
+
+    const sortedJobs = sortJobsByStartTime([...jobs]);
+    
+    // Array para monitorizar os endTimes das salas/servidores
+    const serversEndTimes = [];
+
+    sortedJobs.forEach(job => {
+        let assigned = false;
+
+        for (let i = 0; i < serversEndTimes.length; i++) {
+            if (serversEndTimes[i] <= job.startTime) {
+                job.resourceId = i;
+                serversEndTimes[i] = job.endTime;
+                assigned = true;
+                break;
+            }
+        }
+
+        if (!assigned) {
+            job.resourceId = serversEndTimes.length;
+            serversEndTimes.push(job.endTime);
+        }
+    });
+
+    return sortedJobs;
+}
